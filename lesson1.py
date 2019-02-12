fbd64d516d336c3fadca5e0b0472ace575bc82fc
Kara Stellner <karastellner@Karas-MBP.localdomain>
2019-02-12 11:25:43 -0600

    Lesson on committing files for github

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+first_name = input("Hey, what's your first name?  >> ")
+last_name = input("Oh yeah, and your last name?  >> ")
+
+full_name = f"{first_name} {last_name}"
+
+print("So, your full name is " + full_name + "?")
